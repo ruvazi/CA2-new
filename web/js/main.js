7ea3ac5f3c670bc3333d85c1919c7110b0cdfc75
ERPedersen <emil0srp@gmail.com>
2015-10-09 00:47:15 +0200

    Updated the front-end with the 3 working endpoints

--- web/js/main.js
+++ web/js/main.js
@@ -1,56 +1,125 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // EVENT HANDLERS
+    ////////////////////////////////////////////////////////////////////////////
+    ////////    GET PERSONS ON LOAD
 
-  $("#get-person").on('click', function() {
-    var id = $("#get-person-field").val();
-    personGET('http://localhost:8080/api/person/complete/'+id, personReceived);
-  });
+    apiGET('http://localhost:8084/api/person/complete/', personsLoaded200, personsLoaded404, personsLoaded500);
 
-  $("#create-person").on('click', function() {
-    var fname = $("#create-person-fname").val();
-    var lname = $("#create-person-lname").val();
-    var mail = $("#create-person-email").val();
+    ////////////////////////////////////////////////////////////////////////////
+    ////////    EVENT HANDLERS
+    $("#get-person").on('click', function () {
+        apiGET('http://localhost:8084/api/person/complete/' + $("#get-person-field").val(),
+                singlePerson200, singlePerson404, singlePerson500);
+    });
 
-    var newPerson = {
-      firstname: fname,
-      lastname: lname,
-      email: mail
+    $("#create-person").on('click', function () {
+        var generatedJSON = {
+            firstname: $("#create-person-fname").val(),
+            lastname: $("#create-person-lname").val(),
+            email: $("#create-person-email").val()
+        };
+
+        apiPOST('http://localhost:8084/api/person/', generatedJSON, personCreated200, personCreated404, personCreated500);
+    });
+
+    ////////////////////////////////////////////////////////////////////////////
+    ////////    API FUNCTIONS
+
+    function apiGET(url, resp200, resp404, resp500) {
+        $.ajax(url, {
+            type: 'GET',
+            statusCode: {
+                200: resp200,
+                404: resp404,
+                500: resp500
+            }
+        });
     }
 
-    personPOST('http://localhost:8080/api/person/', newPerson);
-  });
+    function apiPOST(url, obj, resp200, resp404, resp500) {
+        $.ajax(url, {
+            data: JSON.stringify(obj),
+            contentType: 'application/json',
+            type: 'POST',
+            async: true,
+            statusCode: {
+                200: resp200,
+                404: resp404,
+                500: resp500
+            }
+        });
+    }
 
+    ////////////////////////////////////////////////////////////////////////////
+    ////////    CALLBACK FUNCTIONS - GET SINGLE PERSON
 
-  // REST API ACCESSORS
+    function singlePerson200(data) {
+        console.log(data);
+        $("#person").html("");
+        $("#person").html(
+                '<br><p>'
+                + '<b>First Name:</b>' + data.firstname + '<br>'
+                + '<b>Last Name:</b>' + data.lastname + '<br>'
+                +'</p>'
+                );
+    }
+    
+    function singlePerson404() {
+        $("#person").html("");
+        $("#person").html('<p class="text-danger">Could not connect to host. Please try again later</p>');
+    }
+    
+    function singlePerson500() {
+        $("#person").html("");
+        $("#person").html('<p class="text-danger">The person could not be found. Please make sure you are providing a valid ID.</p>');
+    }
+    
+    
 
-  function personGET(url, callback) {
-      $.getJSON(url, function (data) {
-        callback(data);
-      });
-  }
+    ////////////////////////////////////////////////////////////////////////////
+    ////////    CALLBACK FUNCTIONS - CREATE PERSON
 
-  function personPOST(url, obj) {
-      $.ajax(url, {
-        data: JSON.stringify(obj),
-        contentType: 'application/json',
-        type: 'POST',
-        async: true,
-        success: personCreated
-      })
-  }
+    function personCreated200(data) {
+        $("#person-created").html("");
+        $("#person-created").html('<br><p class="text-success">' + data.firstname + ' was succesfully created!' + '</p>');
+        apiGET('http://localhost:8084/api/person/complete/', personsLoaded200, personsLoaded404, personsLoaded500);
+    }
 
-  // CALLBACK FUNCTIONS
+    function personCreated404() {
+        $("#person-created").html("");
+        $("#person-created").html('<br><p class="text-danger">Could not connet to host. Please try again later.</p>');
+    }
 
-  function personReceived(data) {
-      $("#person").html("");
-      $("#person").html(data.firstname + " " + data.lastname);
-  }
+    function personCreated500() {
+        $("#person-created").html("");
+        $("#person-created").html('<br><p class="text-danger">Your person was not created. Please recheck your input.</p>');
+    }
 
-  function personCreated(data) {
-      $("#person-created").html("");
-      $("#person-created").html(data.firstname + " succesfully created!");
-  }
+    ////////////////////////////////////////////////////////////////////////////
+    ////////    CALLBACK FUNCTIONS - LOAD ALL PERSONS
 
+    function personsLoaded200(data) {
+        console.log(data);
+        $("#person-table tbody").html("");
+        for (var i = 0, max = data.length; i < max; i++) {
+            $("#person-table tbody")
+                    .append(
+                            '<tr id="person-' + data[i].id + '">'
+                            + '<td>' + data[i].id + '</td>'
+                            + '<td>' + data[i].firstname + '</td>'
+                            + '<td>' + data[i].lastname + '</td>'
+                            + '<td class="text-center">' + '<a class="delete label label-default" id="delete-' + data[i].id + '">Delete</a>' + '</td>'
+                            + '</tr>'
+                            );
+        }
+    }
+
+    function personsLoaded404(data) {
+        alert('No connection to the server. Please try again later.');
+    }
+
+    function personsLoaded500(data) {
+        alert('There was an error in the server. Please try again later.');
+    }
 });
 
